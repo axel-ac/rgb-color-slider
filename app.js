9096eae3903d748fe6aa3aff75be4a4825978afa
axel-ac <abdullahceylan.axel@gmail.com>
2023-05-18 16:05:06 +0300

    finished

---
+++ app.js
@@ -0,0 +1,33 @@
+const rColor = document.getElementById("red");
+const gColor = document.getElementById("green");
+const bColor = document.getElementById("blue");
+const copyBtn = document.getElementById("copy-btn");
+const result = document.querySelector(".result input");
+const sliders = document.querySelectorAll(".wrapper input[type='range']");
+
+let generateColor = () => {
+  let rColorValue = rColor.value;
+  let gColorValue = gColor.value;
+  let bColorValue = bColor.value;
+
+  let finalColor = `rgb(${rColorValue}, ${gColorValue}, ${bColorValue})`;
+  result.value = finalColor;
+  document.body.style.backgroundColor = finalColor;
+};
+
+sliders.forEach((a) => {
+  a.addEventListener("input", generateColor);
+});
+
+window.addEventListener("load", generateColor);
+
+let copyColorCode = () => {
+  result.select();
+  document.execCommand("copy");
+  copyBtn.innerText = "Copied!";
+  setTimeout(() => {
+    copyBtn.innerText = "Copy Color Code";
+  }, 1000);
+};
+
+copyBtn.addEventListener("click", copyColorCode);
